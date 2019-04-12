17abff78d13f581cab44e6fdba479ec54e4740de
David Vargas <dav793@gmail.com>
2019-04-12 17:54:34 -0600

    Update environment.template.js

--- config/environment.template.js
+++ config/environment.template.js
@@ -7,5 +7,6 @@ module.exports = {
     DB_PORT: 27017,
     DB_NAME: 'express-example',
     DB_AUTH: true,
-    SSL: false
+    SSL: false,
+    JWT_SECRET: 'MY_SECRET_KEY_LepI61NIoK1qG6UYtlO3rms5h1b0qVA7'
 };
